be5f11eb3c53138578dc0fa271502d8fa7a4a008
Harry Michael Jeremy Mead <harry.mead@Harrys-MacBook-Air-8.local>
2017-04-19 20:51:16 +1000

    Beginning to use I2C to communicate between Arduinos

--- mainfiles/unocamera.cpp
+++ mainfiles/unocamera.cpp
@@ -2,42 +2,60 @@
 
 #include "Source/Orientation.h"
 
+#include <Wire.h>
+#include <EasyTransferI2C.h>
+
 bool initEverything();
 
 void dead();
+
+void requestEvent();
+
+EasyTransferI2C ET;
+
+struct SEND_DATA_STRUCTURE {
+  double sendHeading;
+};
+
+SEND_DATA_STRUCTURE myheading;
+
+#define I2C_SLAVE_ADRESS 9
 
 int main()
 {
     if (!initEverything())
         dead();
-    
+
     while (1)
     {
-        Orientation::update();
-        // Orientation::getCompassHeading()
-        // Orientation::getYaw()
-        Serial.println(Orientation::getYaw());
+        myheading.sendHeading = Orientation::getYaw();
+        ET.sendData(I2C_SLAVE_ADRESS);
     }
-    
+
     return 0;
 }
 
 bool initEverything()
 {
     init();
-    
+
+    myheading.sendHeading = 400;
+
+    Wire.begin(8);                // join i2c bus with address #8
+    ET.begin(details(myheading), &Wire);
+
     Serial.begin(115200);
     Serial.println("Initializing");
-    
+
     if (!Orientation::init())
         return false;
-    
+
     return true;
 }
 
 void dead()
 {
     Serial.println(F("R.I.P Arduino"));
-    
+
     while (1);
 }
